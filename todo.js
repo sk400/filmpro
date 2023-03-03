9db491817f7cc235e0f8760f9883b812c1c7ead1
[sk developer] <[skdeveloper101@gmail.com]>
2023-03-03 14:59:40 +0530

    Delete unused files created by create-react-app

---
+++ todo.js
@@ -0,0 +1,15 @@
+// delete unused code, files
+// Install known dependencies
+// File folder structure
+// Layout
+// Sign up & Login page
+// Authentication set up
+// Set up RTK Query
+// Data fetching from tmdb Api
+// Home page
+// Movie Detail Page
+// Set up cloud firestore
+// User profile page
+// Refactoring
+// Favicon and logo
+// deployment
